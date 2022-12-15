02f4709f521dec75f824aa6f4ac56257861e268f
JFSL <lopes.jfsl98@gmail.com>
2022-12-15 00:23:00 +0000

    Get Backlog inside try except

--- utils/backlogHandlers/backlogGet.py
+++ utils/backlogHandlers/backlogGet.py
@@ -28,39 +28,39 @@ def get_backlog_by_user(db_connection, json_request):
     # Establish connection with DB
     cursor = db_connection.cursor()
 
-    # try:
-    user_id = user_id_json["userId"]
+    try:
+        user_id = user_id_json["userId"]
 
-    # Get backlog
-    cursor.execute("SELECT JSON_ARRAYAGG(JSON_OBJECT("
-                   "'movieId', m.Id, "
-                   "'movieName', m.Name, "
-                   "'releaseDate', m.ReleaseDate, "
-                   "'imdbRating', m.ImdbRating, "
-                   "'runTime', m.RunTime, "
-                   "'ageRating', ar.Description, "
-                   "'watchedDate', b.WatchedDate, "
-                   "'statusId', s.Id, "
-                   "'status', s.Description, "
-                   "'userRating', b.Rating "
-                   ")) AS Backlog "
-                   "FROM Backlogs b "
-                   "LEFT JOIN Movies m ON m.Id = b.MovieId "
-                   "LEFT JOIN Statuses s ON s.Id = b.StatusId "
-                   "LEFT JOIN AgeRatings ar ON ar.Id = m.AgeRatingId "
-                   "LEFT JOIN Users u ON u.Id = b.UserId "
-                   f"WHERE u.Id = {user_id}")
+        # Get backlog
+        cursor.execute("SELECT JSON_ARRAYAGG(JSON_OBJECT("
+                       "'movieId', m.Id, "
+                       "'movieName', m.Name, "
+                       "'releaseDate', m.ReleaseDate, "
+                       "'imdbRating', m.ImdbRating, "
+                       "'runTime', m.RunTime, "
+                       "'ageRating', ar.Description, "
+                       "'watchedDate', b.WatchedDate, "
+                       "'statusId', s.Id, "
+                       "'status', s.Description, "
+                       "'userRating', b.Rating "
+                       ")) AS Backlog "
+                       "FROM Backlogs b "
+                       "LEFT JOIN Movies m ON m.Id = b.MovieId "
+                       "LEFT JOIN Statuses s ON s.Id = b.StatusId "
+                       "LEFT JOIN AgeRatings ar ON ar.Id = m.AgeRatingId "
+                       "LEFT JOIN Users u ON u.Id = b.UserId "
+                       f"WHERE u.Id = {user_id}")
 
-    values = cursor.fetchall()
-    if len(values) == 0:
-        return "[]", 200
+        values = cursor.fetchall()
+        if len(values) == 0:
+            return "[]", 200
 
-    str_values = ''.join(str(value[0]) for value in values)
-    backlog_json = json.loads(str_values)
+        str_values = ''.join(str(value[0]) for value in values)
+        backlog_json = json.loads(str_values)
 
-    cursor.close()
-    return backlog_json, 200
+        cursor.close()
+        return backlog_json, 200
 
-    # except Exception:
-    #     cursor.close()
-    #     return jsonify(error='error'), 400
+    except Exception:
+        cursor.close()
+        return jsonify(error='error'), 400
